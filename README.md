31b8997d136cad70bbc1ebe27057a2969468fb5c
Neil Spink <neilspink@gmail.com>
2022-01-16 15:58:21 +0100

    v0

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # furry-eureka-bot
+
+Install python packages
+
+```
+pip3 install -r requirements.txt
+```
+
+Make a .env file
+
+```
+DISCORD_TOKEN=XXX
+```
+
+Run program
+```
+python3 main.py
+```
+
+Adding bot to Discord channel
+
+https://discordapp.com/oauth2/authorize?client_id=CLIENTID&scope=bot
---
+++ main.py
@@ -0,0 +1,36 @@
+# bot.py
+import os
+import discord
+import requests
+import json
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+    if message.content.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)        
+
+client.run(TOKEN)
